4d703e233725dc6c3100c865ab149fbbd2dbecfc
Magomed <abdulaev_21@bk.ru>
2023-01-05 22:05:29 +0300

    FirstBlock

---
+++ FirstBlock/Lecture/Program.cs
@@ -0,0 +1,121 @@
+﻿// Console.Write("Введите имя пользователя: ");
+// string userName = Console.ReadLine()!;
+// Console.Write($"Привет, {userName}!");
+
+
+
+// int a = 3;
+// int b = 5;
+// Console.Write($"Результат: {a + b}");
+
+
+
+// int a = 3;
+// int b = 5;
+// int result = a + b;
+// Console.Write($"Результат: {result}");
+
+
+
+// int a = 10;
+// int b = 5;
+// int result = a / b;
+// Console.Write($"Результат: {result}");
+
+
+
+// int a = 12;
+// int b = 5;
+// int result = a / b;
+// Console.Write($"Результат: {result}"); // Целые числа
+
+
+
+// double a = 12;
+// double b = 5;
+// double result = a / b;
+// Console.Write($"Результат: {result}"); // Вещественные числа
+
+
+
+// int a = new Random().Next(1, 10);
+// int b = new Random().Next(1, 10);
+// int result = a + b;
+// Console.Write($"{a} + {b} = {result}");
+
+
+
+// Console.Write("Введите имя пользователя: ");
+// string userName = Console.ReadLine()!;
+// if (userName == "Маша")
+// {
+//     Console.Write("Ура, это же Маша!");
+// }
+// else
+// {
+//     Console.Write($"Привет, {userName}!");
+// }
+
+
+
+// Console.Write("Введите имя пользователя: ");
+// string userName = Console.ReadLine()!;
+// if (userName.ToLower() == "маша")
+// {
+//     Console.Write("Ура, это же Маша!");
+// }
+// else
+// {
+//     Console.Write($"Привет, {userName}!");
+// }
+
+
+
+// int a = 1;
+// int b = 2;
+// int c = 6;
+// int d = 8;
+// int e = 4;
+// int max = a;
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// if (d > max) max = d;
+// if (e > max) max = e;
+// Console.Write($"Результат: {max}");
+
+
+
+// int xa = 15, ya = 1,
+//     xb = 1, yb = 10,
+//     xc = 30, yc = 10;
+// Console.SetCursorPosition(xa, ya);
+// Console.WriteLine("+");
+// Console.SetCursorPosition(xb, yb);
+// Console.WriteLine("+");
+// Console.SetCursorPosition(xc, yc);
+// Console.WriteLine("+");
+// int x = xa, y = xb;
+// int count = 0;
+// while (count < 1000)
+// {
+//     int number = new Random().Next(0, 3); // 0, 1, 2
+//     if (number == 0)
+//     {
+//         x = (x + xa) / 2;
+//         y = (y + ya) / 2;
+//     }
+//     if (number == 1)
+//     {
+//         x = (x + xb) / 2;
+//         y = (y + yb) / 2;
+//     }
+//     if (number == 2)
+//     {
+//         x = (x + xc) / 2;
+//         y = (y + yc) / 2;
+//     }
+//     Console.SetCursorPosition(x, y);
+//     Console.WriteLine("+");
+//     count++;
+// }
---
+++ FirstBlock/Seminar/Program.cs
@@ -0,0 +1,101 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя)
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+// string input = Console.ReadLine()!;
+// int parsedInput = int.Parse(input);
+// int result = parsedInput * parsedInput;
+// Console.Write(result);
+
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// Console.Write($"Результат: {number * number}");
+
+
+
+// Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+// Console.Write("Введите первое число: ");
+// int a = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите второе число: ");
+// int b = int.Parse(Console.ReadLine()!);
+// if (a == b * b)
+// {
+//     Console.Write("Да");
+// }
+// else
+// {
+//     Console.Write("Нет");
+// }
+
+
+
+// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру
+// 3 -> Среда
+// 5 -> Пятница
+
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// if (number == 1)
+// {
+//     Console.Write("Понедельник");
+// }
+// else if (number == 2)
+// {
+//     Console.Write("Вторник");
+// }
+// else if (number == 3)
+// {
+//     Console.Write("Среда");
+// }
+// else if (number == 4)
+// {
+//     Console.Write("Четверг");
+// }
+// else if (number == 5)
+// {
+//     Console.Write("Пятница");
+// }
+// else if (number == 6)
+// {
+//     Console.Write("Суббота");
+// }
+// else if (number == 7)
+// {
+//     Console.Write("Воскресенье");
+// }
+// else
+// {
+//     Console.Write("Введите число от 1 до 7 включительно");
+// }
+
+
+
+// Задача №5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N
+// 4 -> -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 -> -2, -1, 0, 1, 2
+
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// for (int i = -number; i < number; i++)
+// {
+//     Console.Write($"{i}, ");
+// }
+// Console.Write(number);
+
+
+
+// Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+// Console.Write("Введите трёхзначное число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// Console.Write($"Последнее число - {number % 10}");
